969fe22f6439241bc7a45c7c55db56a0226ff88a
yashnikam09 <124867575+yashnikam09@users.noreply.github.com>
2024-02-19 19:18:41 +0530

    Add files via upload
    CODSOFT TASK 1 REPOSITORY

---
+++ Codsoft1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float num1, num2, result;
+    char operation;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operation. Please choose a valid operation." << endl;
+    }
+
+    return 0;
+}
